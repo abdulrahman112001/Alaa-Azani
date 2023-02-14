5f8ae6ece8633ad689c4be73cd232195502a6eac
abdulrahman112001 <101294492+abdulrahman112001@users.noreply.github.com>
2023-02-15 00:22:27 +0200

    push

--- f
+++ f
@@ -1,24 +1,16 @@
 /* 
+-Enable light mode and dark mode  //
+-loading // 
+-edit scroll down  >> done 
+-call + whatsapp  
+-select text doing  highlight in main color  /// 
 
 
 
--Enable light mode and dark mode 
 
-
--loading
-
-
-
--edit scroll down 
--call + whatsapp
--social media in  home page  show in 3 icons in hover this icon 
--create new page ==> (job )
-
-
-
-- select text doing  highlight in main color 
-
-
+-enable lang AR  //
+-create new page ==> (job ) --> 
+-social media in  home page  show in 3 icons in hover this icon  //  edit  in 3 icons  
 
 
 
--- js/app.js
+++ js/app.js
@@ -1,4 +1,101 @@
 /*TODO: TypeWriting animation*/
+  let dark = document.querySelector(".moon");
+
+  if (localStorage.getItem("darkMode") === null) {
+    localStorage.setItem("darkMode", "false");
+  }
+
+  function checkState() {
+    if (localStorage.getItem("darkMode") === "true") {
+      document.body.classList.add("dark");
+      dark.src = "imgs/sun.png";
+    } else {
+      document.body.classList.remove("dark");
+      dark.src = "imgs/moon.png";
+    }
+  }
+  checkState();
+
+  dark.onclick = function () {
+    localStorage.getItem("darkMode") === "true"
+      ? localStorage.setItem("darkMode", "false")
+      : localStorage.setItem("darkMode", "true");
+    checkState();
+  };
+
+var menuButton = $(".menu-button"),
+  menuTop,
+  menuLeft,
+  topPos,
+  leftPos,
+  halfScreenVert,
+  halfScreenHor;
+
+function checkMenuPos() {
+  menuTop = menuButton.offset().top + menuButton.height() / 2;
+  menuLeft = menuButton.offset().left + menuButton.width() / 2;
+  topPos = menuButton.offset().top;
+  leftPos = menuButton.offset().left;
+  halfScreenVert = $(window).height() / 2;
+  halfScreenHor = $(window).width() / 2;
+
+  if (menuTop <= halfScreenVert && menuLeft <= halfScreenHor) {
+    menuButton.addClass("top-left");
+  } else {
+    menuButton.removeClass("top-left");
+  }
+
+  if (menuTop <= halfScreenVert && menuLeft > halfScreenHor) {
+    menuButton.addClass("top-right");
+  } else {
+    menuButton.removeClass("top-right");
+  }
+
+  if (menuTop > halfScreenVert && menuLeft <= halfScreenHor) {
+    menuButton.addClass("bottom-left");
+  } else {
+    menuButton.removeClass("bottom-left");
+  }
+
+  if (menuTop > halfScreenVert && menuLeft > halfScreenHor) {
+    menuButton.addClass("bottom-right");
+  } else {
+    menuButton.removeClass("bottom-right");
+  }
+}
+
+if (
+  localStorage.getItem("menuTop") !== null &&
+  localStorage.getItem("menuLeft") !== null
+) {
+  var menuLastTop = parseInt(localStorage.getItem("menuTop")),
+    menuLastLeft = parseInt(localStorage.getItem("menuLeft"));
+
+  menuButton.css({ bottom: "auto", top: menuLastTop, left: menuLastLeft });
+
+  checkMenuPos();
+}
+
+menuButton.draggable({
+  drag: function (event, ui) {
+    localStorage.setItem("menuTop", topPos);
+    localStorage.setItem("menuLeft", leftPos);
+
+    checkMenuPos();
+  },
+  cancel: "li",
+});
+
+menuButton.click(function (event) {
+  if ($(event.target).is("ul")) {
+    menuButton.toggleClass("active");
+  }
+});
+
+$(window).resize(function () {
+  checkMenuPos();
+});
+
 let TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
@@ -128,14 +225,15 @@ let swiper = new Swiper(".mySwiper", {
 /*TODO: partner swiper */
 let swiper2 = new Swiper(".mySwiper2", {
   slidesPerView: 3,
+  loop:true,
   grid: {
     rows: 3,
   },
   spaceBetween: 30,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   breakpoints: {
     0: {
       slidesPerView: 1,
@@ -431,3 +529,5 @@ jSuites.dropdown(document.getElementById("dropdown"), {
 
 document.querySelector("#dropdown .jdropdown-container-header input").setAttribute("required", "required");
 document.querySelector("#dropdown .jdropdown-container-header input").setAttribute("name", "country");
+
+
